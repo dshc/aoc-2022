0434f4577ab9ee9b0f1bbbd263478b040b438ba8
David Chang <davidshchang@gmail.com>
2022-12-01 10:38:00 -0600

    day01

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,3 +1,13 @@
+#![allow(dead_code)]
+
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+const INPUT_PATH_REAL: &str = "./inputs/011real.txt";
+const INPUT_PATH_TEST: &str = "./inputs/011test.txt";
+
 pub fn solve() {
     let part1 = part1();
     match part1 {
@@ -12,10 +22,55 @@ pub fn solve() {
     }
 }
 
-fn part1() -> Option<String> {
-    return Some(String::from("SOME TEMPORARY STRING"));
+fn part1() -> Option<u32> {
+    let mut max: u32 = 0;
+    let mut curr_elf: u32 = 0;
+
+    get_line_iter(INPUT_PATH_REAL).for_each(|x| match x.unwrap().trim() {
+        "" => {
+            if curr_elf > max {
+                max = curr_elf
+            }
+            curr_elf = 0;
+        }
+        cals => curr_elf += cals.parse::<u32>().unwrap(),
+    });
+
+    if curr_elf != 0 && curr_elf > max {
+        max = curr_elf;
+    }
+
+    return Some(max);
 }
 
-fn part2() -> Option<String> {
-    return None;
+fn part2() -> Option<u32> {
+    let mut cals_by_elf = Vec::new();
+    let mut curr_elf = 0;
+
+    get_line_iter(INPUT_PATH_REAL).for_each(|x| match x.unwrap().trim() {
+        "" => {
+            cals_by_elf.push(curr_elf);
+            curr_elf = 0;
+        }
+        cals => curr_elf += cals.parse::<u32>().unwrap(),
+    });
+
+    if curr_elf != 0 {
+        cals_by_elf.push(curr_elf);
+    }
+
+    cals_by_elf.sort();
+
+    let mut sum = 0;
+    for _ in 0..3 {
+        sum += cals_by_elf.pop().unwrap();
+    }
+
+    return Some(sum);
+}
+
+fn get_line_iter(path: &str) -> io::Lines<io::BufReader<File>> {
+    let file = File::open(path).unwrap();
+    let reader = io::BufReader::new(file);
+    return reader.lines();
 }
