8dd0b86eec536153fa39c1d477f3d5c95ecaea68
David Chang <davidshchang@gmail.com>
2022-12-14 08:38:28 -0600

    day14 complete

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -15,30 +15,139 @@ fn part1(input: &str) -> usize {
         })
         .collect_vec();
 
-    let mut grid = vec![vec![false; 200]; 200];
+    let mut grid = vec![vec![Material::Air; 200]; 200];
     all_rocks.iter().for_each(|coord| {
-        grid[coord.x][coord.y] = true;
+        grid[coord.x][coord.y] = Material::Rock;
     });
 
-    for y in 0..200 {
-        for x in 0..200 {
-            if y == 0 && x == 100 {
-                print!("+");
+    // start dropping sand
+    let mut counter = 0;
+    loop {
+        let mut curr_coord = Coord { x: 100, y: 0 };
+
+        loop {
+            if curr_coord.y >= 199 {
+                // _print_grid_part1(&grid);
+                return counter;
             }
-            else if grid[x][y] {
+
+            // check directly below
+            if let Material::Air = grid[curr_coord.x][curr_coord.y + 1] {
+                curr_coord.y += 1;
+            }
+            // check down to left
+            else if let Material::Air = grid[curr_coord.x - 1][curr_coord.y + 1] {
+                curr_coord.x -= 1;
+                curr_coord.y += 1;
+            }
+            // check down to right
+            else if let Material::Air = grid[curr_coord.x + 1][curr_coord.y + 1] {
+                curr_coord.x += 1;
+                curr_coord.y += 1;
+            } else {
+                grid[curr_coord.x][curr_coord.y] = Material::Sand;
+                counter += 1;
+                break;
+            }
+        }
+    }
+}
+
+fn part2(input: &str) -> usize {
+    let all_rocks = get_all_rocks(input)
+        .into_iter()
+        .map(|c| Coord {
+            x: c.x,
+            y: c.y,
+        })
+        .collect_vec();
+
+    let floor = all_rocks
+            .iter()
+            .max_by(|a, b| a.y.cmp(&b.y))
+            .unwrap()
+            .y + 2;
+
+    let mut grid = vec![vec![Material::Air; 200]; 1000];
+    all_rocks.iter().for_each(|coord| {
+        grid[coord.x][coord.y] = Material::Rock;
+    });
+    for x in 0..1000 {
+        grid[x][floor] = Material::Rock;
+    }
+
+    // start dropping sand
+    let mut counter = 0;
+    loop {
+        let mut curr_coord = Coord { x: 500, y: 0 };
+
+        loop {
+            if let Material::Sand = grid[500][0] {
+                // _print_grid_part2(&grid);
+                return counter;
+            }
+
+            // check directly below
+            if let Material::Air = grid[curr_coord.x][curr_coord.y + 1] {
+                curr_coord.y += 1;
+            }
+            // check down to left
+            else if let Material::Air = grid[curr_coord.x - 1][curr_coord.y + 1] {
+                curr_coord.x -= 1;
+                curr_coord.y += 1;
+            }
+            // check down to right
+            else if let Material::Air = grid[curr_coord.x + 1][curr_coord.y + 1] {
+                curr_coord.x += 1;
+                curr_coord.y += 1;
+            } else {
+                grid[curr_coord.x][curr_coord.y] = Material::Sand;
+                counter += 1;
+                break;
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Material {
+    Air,
+    Rock,
+    Sand,
+}
+
+fn _print_grid_part2(grid: &Vec<Vec<Material>>) {
+    for y in 0..200 {
+        for x in 0..1000 {
+            if y == 0 && x == 500 {
+                print!("+");
+            } else if let Material::Rock = grid[x][y] {
                 print!("#");
+            } else if let Material::Sand = grid[x][y] {
+                print!("o");
             } else {
                 print!(".");
             }
         }
         println!();
     }
-
-    0
 }
 
-fn part2(input: &str) -> usize {
-    0
+fn _print_grid_part1(grid: &Vec<Vec<Material>>) {
+    for y in 0..200 {
+        for x in 0..200 {
+            if y == 0 && x == 100 {
+                print!("+");
+            } else if let Material::Rock = grid[x][y] {
+                print!("#");
+            } else if let Material::Sand = grid[x][y] {
+                print!("o");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
 }
 
 fn get_all_rocks(input: &str) -> Vec<Coord> {
@@ -106,5 +215,11 @@ mod tests {
     fn part1_example_test() {
         let input = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9";
         assert_eq!(part1(input), 24);
+    }
+
+    #[test]
+    fn part2_example_test() {
+        let input = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9";
+        assert_eq!(part2(input), 93);
     }
 }
